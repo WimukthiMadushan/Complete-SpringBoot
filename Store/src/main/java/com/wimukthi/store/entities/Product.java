dcffe4276c6ff65fd5ac42f7085b4ef9a63930a9
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-05 16:19:01 +0530

    create products entity and make one to many reation with category

---
+++ Store/src/main/java/com/wimukthi/store/entities/Product.java
@@ -0,0 +1,35 @@
+package com.wimukthi.store.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+
+}
