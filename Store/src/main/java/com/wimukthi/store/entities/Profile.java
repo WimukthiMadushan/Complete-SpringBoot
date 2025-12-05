6dbbbd8ab09d573b944056becd87fbe13044ee1a
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-05 13:19:58 +0530

    define entities

---
+++ Store/src/main/java/com/wimukthi/store/entities/Profile.java
@@ -0,0 +1,29 @@
+package com.wimukthi.store.entities;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "profiles")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "phone_number")
+    private String phone_number;
+
+    @Column(name = "date_of_birth")
+    private LocalDate date_of_birth;
+
+    @Column(name = "loyalty_points")
+    private Integer loyalty_points;
+}
