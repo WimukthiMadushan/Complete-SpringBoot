608bdc3e83ed98ac3983a3987685bf73fc155486
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 10:49:11 +0530

    create resources using POST request

---
+++ spring-api/src/main/java/com/codewithmosh/store/dto/RegisterUserRequest.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserRequest {
+
+    private String name;
+    private String email;
+    private String password;
+}
--- spring-api/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ spring-api/src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.mappers;
 
+import com.codewithmosh.store.dto.RegisterUserRequest;
 import com.codewithmosh.store.dto.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
@@ -7,4 +8,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto userToUserDto(User user);
+    User toEntity(RegisterUserRequest request);
 }
