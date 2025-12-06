87342b4af523a232157dbf016be56a347322611e
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 09:12:27 +0530

    works on the query parameters

--- spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -4,12 +4,15 @@ import com.codewithmosh.store.dto.UserDto;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 
 @RestController("/users")
@@ -20,8 +23,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping()
-    public List<UserDto> getAllUsers() {
-        return userRepository.findAll().stream()
+    public List<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sortBy") String sortBy
+    ) {
+        if(!Set.of("name", "email").contains(sortBy)){
+            sortBy = "name";
+        }
+        return userRepository.findAll(Sort.by(sortBy).ascending()).stream()
                 .map(userMapper::userToUserDto)
                 .toList();
     }
