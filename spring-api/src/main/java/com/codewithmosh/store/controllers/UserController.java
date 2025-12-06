a293ef18028539a09eacfa290bbb96d32f1d4b6d
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 12:51:04 +0530

    Global Error handler

--- spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -11,13 +11,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 
@@ -100,16 +97,5 @@ public class UserController {
         userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleValidationErrors(
-            MethodArgumentNotValidException exception
-    ){
-        var errors = new HashMap<String,String>();
-        exception.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-        return ResponseEntity.badRequest().body(errors);
 
-
-    }
 }
