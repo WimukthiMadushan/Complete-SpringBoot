9938e2fd5e0a82f720f747c7450f02a387030f67
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 13:22:23 +0530

    initialize capstone project 01

---
+++ capstone-project-01/src/main/java/com/wimukthi/capstoneproject01/CapstoneProject01Application.java
@@ -0,0 +1,13 @@
+package com.wimukthi.capstoneproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProject01Application.class, args);
+    }
+
+}
---
+++ capstone-project-01/src/test/java/com/wimukthi/capstoneproject01/CapstoneProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimukthi.capstoneproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
