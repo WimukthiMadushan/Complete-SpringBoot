c8205b295c39d7e0cf590f7ece50c2532b7c760c
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 08:46:38 +0530

    use data transfer objects for stabale data commiunication between applications

--- spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,14 +1,14 @@
 package com.codewithmosh.store.controllers;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.dto.UserDto;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController("/users")
@@ -18,15 +18,18 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping()
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserDto(user.getId(), user.getName(),  user.getEmail()))
+                .toList();
     }
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable long id) {
         var user =  userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
