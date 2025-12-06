06211ae4b7810d0670448e813e6d2c86264357b9
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 13:00:20 +0530

    implement custom validation

--- spring-api/src/main/java/com/codewithmosh/store/dto/RegisterUserRequest.java
+++ spring-api/src/main/java/com/codewithmosh/store/dto/RegisterUserRequest.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.dto;
 
+import com.codewithmosh.store.validation.Lowercase;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -14,6 +15,7 @@ public class RegisterUserRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email Should valid")
+    @Lowercase(message = "Email must be in lowercase")
     private String email;
 
     @NotBlank(message = "Password is Required")
