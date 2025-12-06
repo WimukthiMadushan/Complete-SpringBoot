12a3e7ab4d12f6a65d248e21f10b52fa1833ee29
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 18:22:02 +0530

    created entities

---
+++ capstone-project-01/src/main/java/com/wimukthi/capstoneproject01/entities/Product.java
@@ -0,0 +1,33 @@
+package com.wimukthi.capstoneproject01.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+}
