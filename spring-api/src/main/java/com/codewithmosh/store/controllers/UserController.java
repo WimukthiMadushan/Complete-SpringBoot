e785fcbb8d859ceacc9c4745ac305e9e765808c7
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-06 11:52:29 +0530

    handling validation errors

--- spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ spring-api/src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -11,10 +11,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -96,6 +99,17 @@ public class UserController {
         user.setPassword(request.getNewPassword());
         userRepository.save(user);
         return ResponseEntity.noContent().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ){
+        var errors = new HashMap<String,String>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+
 
     }
 }
