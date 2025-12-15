cc3d2dfc5faf8a252b8c59c50c44a46a5087ab92
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-15 08:42:55 +0530

    hash password

--- spring-api/src/main/java/com/codewithmosh/store/Config/SecurityConfig.java
+++ spring-api/src/main/java/com/codewithmosh/store/Config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.codewithmosh.store.Config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
